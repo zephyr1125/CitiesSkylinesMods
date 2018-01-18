0502a56476cf65796f16b664faee26a841f1ced4
Zephyr Zhang <zephyr1125@gmail.com>
2018-01-18 21:15:51 +0800

    首次提交

---
+++ TestMod/Examples/UnlockAll/Source/UnlockAll.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using ICities;
+using UnityEngine;
+
+namespace UnlockAllMod {
+
+    public class UnlockAll : IUserMod {
+
+        public string Name {
+            get { return "Unlock All"; }
+        }
+
+        public string Description {
+            get { return "Progression milestones are always unlocked"; }
+        }
+
+    }
+
+    public class UnlockAllMilestones : MilestonesExtensionBase {
+
+        public override void OnRefreshMilestones() {
+            milestonesManager.UnlockMilestone("Basic Road Created");
+        }
+
+        public override int OnGetPopulationTarget(int originalTarget, int scaledTarget) {
+            return 0;
+        }
+
+    }
+
+}
---
+++ TestMod/ZephyrTest/TestOne.cs
@@ -0,0 +1,25 @@
+﻿using ICities;
+
+namespace ZephyrTest
+{
+    public class TestOne : IUserMod
+    {
+        public string Name
+        {
+            get { return "太昊的Mod"; }
+        }
+
+        public string Description
+        {
+            get { return "太昊的Mod的测试"; }
+        }
+    }
+
+    public class TestOneResource : ResourceExtensionBase
+    {
+        public override void OnAfterResourcesModified(int x, int z, NaturalResource type, int amount)
+        {
+            
+        }
+    }
+}
